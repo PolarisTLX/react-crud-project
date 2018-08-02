432818c83ff5d7d83b1b917da214d8e98fdce35a
PolarisTLX <paul.a.rail@gmail.com>
2018-08-02 09:56:36 -0400

    reviewing react basics
    Co-authored-by: Kyle Lemon <james.kyle.lemon@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import MyComponent from './MyComponent';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: 'Appetizer Title'
+    };
+
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    this.setState({
+      title: 'New appetizer title'
+    });
+  }
+  onSubmit(event) {
+    event.preventDefault();
+    console.log(this.random.value);
+  }
+
   render() {
+    const title = "This is my next app";
+    const list = ["item 1", "item 2", "item3"]
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>{title}</h1>
+        <form onSubmit={this.onSubmit}>
+          <input onChange={this.onChange} ref={input => this.random = input} />
+        </form>
+
+        <h1 onClick={this.onClick}>{this.state.title}</h1>
+
+        <div onClick={this.onClick}>Click Me</div>
+        < MyComponent />
+
       </div>
     );
   }
---
+++ src/MyComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class MyComponent extends Component {
+  render() {
+    return (
+      <div className="component">
+        <h1>This is a separate component!</h1>
+
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
