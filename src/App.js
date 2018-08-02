19ce7c18655bd3b19617749cc2352feb5f562b2c
Kyle Lemon <james.kyle.lemon@gmail.com>
2018-08-02 10:57:43 -0400

    lifecycle methods
    Co-authored-by: Paul Rail <paul.a.rail@gmail.com>

--- src/App.js
+++ src/App.js
@@ -8,21 +8,49 @@ class App extends Component {
     super(props);
 
     this.state = {
-      title: 'Appetizer Title'
+      title: 'Appetizer Title',
+      name: 'Appetizer Name',
+      mainTitle: 'Main Title',
+      checked: true,
+      shouldRenderTitle: false
     };
 
     this.onSubmit = this.onSubmit.bind(this);
     this.onClick = this.onClick.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.updateName = this.updateName.bind(this);
+    this.updateCheck = this.updateCheck.bind(this);
   }
 
   onClick() {
     this.setState({
-      title: 'New appetizer title'
+      title: 'New appetizer title',
+      name: 'New appetizer name'
+    });
+  }
+  handleClick() {
+    this.setState({
+      mainTitle: 'Changed by clicking'
     });
   }
   onSubmit(event) {
     event.preventDefault();
     console.log(this.random.value);
+  }
+  updateName(event) {
+    this.setState({
+      name: event.target.value
+    });
+  }
+  updateCheck() {
+    this.setState({
+      checked: !this.state.checked
+    });
+  }
+
+  renderTitle() {
+    if (!this.state.shouldRenderTitle) {return null;}
+    return <h1>Another Title</h1>
   }
 
   render() {
@@ -31,16 +59,33 @@ class App extends Component {
 
     return (
       <div className="App">
-        <h1>{title}</h1>
+        {
+          this.state.shouldRenderTitle
+          ? <h1>Another Title</h1>
+          : null
+        }
+        <h1>{this.state.mainTitle}</h1>
         <form onSubmit={this.onSubmit}>
           <input onChange={this.onChange} ref={input => this.random = input} />
         </form>
 
         <h1 onClick={this.onClick}>{this.state.title}</h1>
 
-        <div onClick={this.onClick}>Click Me</div>
-        < MyComponent />
-
+        <div onClick={this.handleClick}>Click Me</div>
+        < MyComponent
+          title={this.state.title}
+          name={this.state.name}
+          onClick={this.onClick}
+        />
+        <input
+          value={this.state.name}
+          onChange={this.updateName}
+        />
+        <input
+          type="checkbox"
+          onChange={this.updateCheck}
+          checked={this.state.checked}
+        />
       </div>
     );
   }
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,14 +1,51 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+  name: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+  title: PropTypes.string.isRequired
+};
+
+const defaultProps = {
+  name: 'Default name of App'
+}
 
 class MyComponent extends Component {
+  componentWillMount() {
+    //AJAX requests
+    console.log('Component is about to mount')
+  }
+  componentDidMount() {
+    //focus field after component loads
+    console.log('Component mounted')
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log(this.props)
+    console.log(nextProps)
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log(this.props)
+    console.log(prevProps)
+  }
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   //return true;
+  //   //return false;
+  // }
   render() {
+    const {title, name, onClick} = this.props;
     return (
       <div className="component">
-        <h1>This is a separate component!</h1>
-
+        <h1>Title: {title}</h1>
+        <h2>Name: {name}</h2>
+        <div onClick={onClick}>Click me</div>
       </div>
     );
   }
 }
+
+MyComponent.propTypes = propTypes;
+MyComponent.defaultProps = defaultProps;
 
 export default MyComponent;
